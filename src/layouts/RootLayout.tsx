7d9f48967ff2f68aafefd26c6ea23ee9823cc7a6
arafatDU <bsse1435@iit.du.ac.bd>
2025-03-11 17:23:10 +0600

    Background Shape added

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/Footer"
 const RootLayout = () => {
   return (
     <>
-     <div className="min-h-[100vh] flex flex-col overflow-hidden">
+     <div className="relative isolate min-h-[100vh] flex flex-col overflow-hidden">
         <Header />
 
         <main className="grow grid grid-cols-1 items-center pt-36 pb-16">
@@ -13,6 +13,12 @@ const RootLayout = () => {
         </main>
 
         <Footer />
+
+        {/* Background shapes */}
+        <div className="bg-primary/20 absolute top-20 left-0 w-80
+        h-10 rotate-45 origin-top-left blur-3xl"></div>
+        <div className="bg-primary/20 absolute top-20 left-0 w-80
+        h-10 rotate-45 origin-top-right blur-3xl"></div>
      </div>
     </>
   )
