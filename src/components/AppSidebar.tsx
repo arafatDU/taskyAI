8b0db22131e7d4291f24e07444006d307f2d6153
arafatDU <bsse1435@iit.du.ac.bd>
2025-03-12 14:43:59 +0600

    Sidebar completed

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -3,11 +3,23 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarGroupLabel,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuBadge,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar"
 import Logo from "@/components/Logo"
 import { Link } from "react-router"
 import { UserButton } from "@clerk/clerk-react"
+import { ChevronRight, CirclePlus, Plus } from "lucide-react"
+
+import { SIDEBAR_LINKS } from "@/constants"
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible"
+import { Tooltip, TooltipTrigger, TooltipContent } from "@/components/ui/tooltip"
 
 const AppSidebar = () => {
   return (
@@ -22,7 +34,79 @@ const AppSidebar = () => {
       </SidebarHeader>
       <SidebarContent>
         <SidebarGroup />
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {/* Task create button */}
+              <SidebarMenuItem>
+                <SidebarMenuButton className="!text-primary hover:bg-primary/10">
+                  <CirclePlus /> Add Task
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+
+              {/* Sidebar links */}
+              {
+                SIDEBAR_LINKS.map((item, index) => (
+                  <SidebarMenuItem key={index}>
+                    <SidebarMenuButton asChild className="hover:bg-primary/10">
+                      <Link to={item.href}>
+                        <item.icon />
+                        <span>{item.label}</span>
+                      </Link>
+                    </SidebarMenuButton>
+
+                    <SidebarMenuBadge>0</SidebarMenuBadge>
+                  </SidebarMenuItem>
+                ))
+              }
+            </SidebarMenu>
+          </SidebarGroupContent>
         <SidebarGroup />
+
+        {/* All projects */}
+        <Collapsible 
+          defaultOpen 
+          className="group/collapsible"
+        >
+          <SidebarGroup>
+            <SidebarGroupLabel 
+              asChild
+              className="text-muted-foreground text-sm
+              text-sidebar-foreground hover:bg-primary/10 hover:text-white"
+            >
+              <CollapsibleTrigger >
+                <ChevronRight className="me-2 transition-transform 
+                group-data-[state=open]/collapsible:rotate-90" /> 
+                Projects
+              </CollapsibleTrigger>
+            </SidebarGroupLabel>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <SidebarGroupAction aria-label="Add Project">
+                  <Plus /> 
+                </SidebarGroupAction>
+              </TooltipTrigger>
+
+              <TooltipContent side="right">
+                Add Project
+              </TooltipContent>
+            </Tooltip>
+            
+
+            <CollapsibleContent>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  <SidebarMenuItem>
+                    <p className="text-muted-foreground text-sm p-2">
+                      Click + to add some projects
+                    </p>
+                  </SidebarMenuItem>
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </CollapsibleContent>
+          </SidebarGroup>
+        </Collapsible>
+        
       </SidebarContent>
       <SidebarFooter>
         <UserButton 
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,15 +1,21 @@
 import AppSidebar from "@/components/AppSidebar"
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { Outlet } from "react-router"
+import { TooltipProvider } from "@/components/ui/tooltip"
 
 
 const AppLayout = () => {
   return (
     <SidebarProvider>
-      <AppSidebar />
-      <SidebarTrigger />
-      <div>Applayout</div>
-      <Outlet />
+      <TooltipProvider 
+        delayDuration={500}
+        disableHoverableContent
+      >
+        <AppSidebar />
+        <SidebarTrigger />
+        <div>Applayout</div>
+        <Outlet />
+      </TooltipProvider>
     </SidebarProvider>
   )
 }
